0365e74609edd143fff64007b795302a81906611
gentry <gma23@txstate.edu>
2022-05-24 13:05:36 -0500

    removing tensorflow autoencoder

--- src/compare_nnclr.py
+++ src/compare_nnclr.py
@@ -17,6 +17,7 @@ import umap
 import numpy as np
 from utils.import_datasets import get_unimib_data
 from utils.ts_feature_toolkit import get_features_for_set
+from utils.build_AE import build_AE, get_trained_AE
 
 if __name__ == '__main__':
     X, y, labels = get_unimib_data()
@@ -24,5 +25,12 @@ if __name__ == '__main__':
     print('Shape of X: ', X.shape)
     flattened_X = np.array([np.linalg.norm(i, axis=0) for i in X])
     print('Shape of flattened X: ', flattened_X.shape)
-    trad_features = get_features_for_set(flattened_X)
-    print('Shape of computed features: ', trad_features.shape)
+
+    #trad_features = get_features_for_set(flattened_X)
+    #print('Shape of computed features: ', trad_features.shape)
+
+    auto_enc = build_AE(flattened_X)
+    ae_features = auto_enc.predict(flattened_X)
+    print('Shape of autoencoded features: ', ae_features.shape)
+
+
