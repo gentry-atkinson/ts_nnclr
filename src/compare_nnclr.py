dfd986122490388b043f5eed71dd1ac450a289f4
gentry <gma23@txstate.edu>
2022-05-27 12:18:13 -0500

    added 3d plotting

--- src/compare_nnclr.py
+++ src/compare_nnclr.py
@@ -15,14 +15,18 @@
 
 import umap
 import numpy as np
+from matplotlib import pyplot as plt
 from utils.import_datasets import get_unimib_data
 from utils.ts_feature_toolkit import get_features_for_set as get_trad_features
 from utils.ae_feature_learner import get_features_for_set as get_ae_features
 from utils.nnclr_feature_learner import get_features_for_set as get_nnclr_features
 
-run_trad = False
+run_trad = True
 run_ae = False
-run_nnclr = True
+run_nnclr = False
+
+umap_neighbors = 15
+umap_dim = 3
 
 if __name__ == '__main__':
     X, y, labels = get_unimib_data()
@@ -36,10 +40,26 @@ if __name__ == '__main__':
     if run_trad:
         trad_features = get_trad_features(np.reshape(flattened_X, (flattened_X.shape[0], flattened_X.shape[1])))
         print('Shape of computed features: ', trad_features.shape)
+        reducer = umap.UMAP(n_neighbors=umap_neighbors, n_components=umap_dim)
+        embedding = reducer.fit_transform(trad_features)
+        print('Shape of UMAP representation: ', embedding.shape)
+        plt.figure()
+        if umap_dim==2:
+            plt.scatter(embedding[:,0], embedding[:,1], c=y)
+        else:
+            ax = plt.axes(projection ="3d")
+            ax.scatter(embedding[:,0], embedding[:,1], embedding[:,2], c=y)
+        plt.show()
 
     if run_ae:
         ae_features = get_ae_features(flattened_X, with_visual=False)
         print('Shape of autoencoded features: ', ae_features.shape)
+        reducer = umap.UMAP()
+        embedding = reducer.fit_transform(ae_features)
+        print('Shape of UMAP representation: ', embedding.shape)
+        plt.figure()
+        plt.scatter(embedding[:,0], embedding[:,1], c=y)
+        plt.show()
 
     if run_nnclr:
         nnclr_feature = get_nnclr_features(flattened_X)
