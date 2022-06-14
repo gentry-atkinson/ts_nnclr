01a790c56dc4002e4b2e7df0c5597ee9129fea20
Gentry Atkinson <gma23@txstate.edu>
2022-06-14 18:30:21 -0500

    getting simclr into the mix

---
+++ src/utils/simclr_feature_learner.py
@@ -0,0 +1,6 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 14 June, 2022
+#Build and trained a self-supervised feature extractor using
+#  SimCLR, following:
+#  https://github.com/iantangc/ContrastiveLearningHAR
