5a91fbf1efe57d14789639535ba3470d837d46be
Gentry Atkinson <gma23@txstate.edu>
2022-06-22 16:14:34 -0500

    working on a file of clustering metrics

---
+++ src/utils/clustering_metrics.py
@@ -0,0 +1,12 @@
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 22 June, 2022
+#A collection of useful clustering metrics
+
+import numpy
+
+#Dunn Index
+#https://en.wikipedia.org/wiki/Dunn_index
+#DI = min(distance between any two clusters)/max(size of one cluster)
+def dunn_index(instances, clusters):
+    pass
