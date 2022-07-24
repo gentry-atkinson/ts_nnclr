8e4be9e6b48a6cecfc29c8cea565453b07b1c2bc
gentry <gma23@txstate.edu>
2022-07-24 14:26:44 -0500

    added time shift

--- src/lightly_plus_time/lightly/transforms/signalShift.py
+++ src/lightly_plus_time/lightly/transforms/signalShift.py
@@ -17,3 +17,16 @@ class AmplitudeShift(object):
             return torch.Tensor([i+self.shift for i in signal])
         else:
             return signal
+
+class TimeShift(object):
+    def __init__(self, prob: float = 0.5, shift: int = 20):
+        self.prob = prob
+        self.shift = shift
+
+    def __call__(self, signal):
+        #Set some samples to 0 with random chance
+        signalLength = len(signal)
+        if np.random.random_sample() < self.prob:
+            return torch.Tensor([signal[(i+self.shift)%signalLength] for i in range(signalLength)])
+        else:
+            return signal
