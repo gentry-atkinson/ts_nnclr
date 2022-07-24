d4abe3c6fc8e3cfccfd6fb15f20cf1a9828c6da7
gentry <gma23@txstate.edu>
2022-07-24 14:25:01 -0500

    added amp shift

--- src/lightly_plus_time/lightly/transforms/signalShift.py
+++ src/lightly_plus_time/lightly/transforms/signalShift.py
@@ -1,21 +1,19 @@
 #Author: Gentry Atkinson
 #Organization: Texas University
 #Data: 24 July, 2022
-#Add a small, random variance to each sample
+#Translate a signal up, down, left, or right
 
 import numpy as np
 import torch
-from random import gauss, seed
 
-class GaussianNoise(object):
-    def __init__(self, prob: float = 0.5, sigma: float = 0.1):
+class AmplitudeShift(object):
+    def __init__(self, prob: float = 0.5, shift: float = 0.5):
         self.prob = prob
-        self.signma = sigma
+        self.shift = shift
 
     def __call__(self, signal):
         #Set some samples to 0 with random chance
         if np.random.random_sample() < self.prob:
-            seed()
-            return torch.Tensor([gauss(i, self.sigma) for i in signal])
+            return torch.Tensor([i+self.shift for i in signal])
         else:
             return signal
