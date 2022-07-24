573badef8fbd81026941e05724b8391bca6ab622
gentry <gma23@txstate.edu>
2022-07-24 14:29:06 -0500

    added flip

--- src/lightly_plus_time/lightly/transforms/signalShift.py
+++ src/lightly_plus_time/lightly/transforms/signalShift.py
@@ -30,3 +30,15 @@ class TimeShift(object):
             return torch.Tensor([signal[(i+self.shift)%signalLength] for i in range(signalLength)])
         else:
             return signal
+
+class Flip(object):
+    def __init__(self, prob: float = 0.5):
+        self.prob = prob
+
+    def __call__(self, signal):
+        #Set some samples to 0 with random chance
+        signalLength = len(signal)
+        if np.random.random_sample() < self.prob:
+            return torch.Tensor([-1*x for x in signal])
+        else:
+            return signal
