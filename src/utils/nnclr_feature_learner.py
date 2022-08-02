4413798b3b7ff4c6bf2d8860da210bd92f18ea87
Gentry Atkinson <gma23@txstate.edu>
2022-08-02 14:24:47 -0500

    getting twister added

--- src/utils/nnclr_feature_learner.py
+++ src/utils/nnclr_feature_learner.py
@@ -21,7 +21,7 @@ from lightly_plus_time.lightly.data import LightlyDataset
 from lightly_plus_time.lightly.loss import NTXentLoss
 from lightly_plus_time.ts_utils.ts_dataloader import UCR2018
 
-MAX_EPOCHS = 10
+MAX_EPOCHS = 100
 PATIENCE = 5
 
 def get_features_for_set(X, y=None, with_visual=False, with_summary=False, returnModel=False):
