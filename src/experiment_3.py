8a51438483f99480544003f70c6565f9c45bde39
gentry <gma23@txstate.edu>
2022-08-05 20:06:29 -0500

    added noise generator to exp 3

--- src/experiment_3.py
+++ src/experiment_3.py
@@ -23,8 +23,9 @@ run_simclr = True
 from load_data_time_series_dev.HAR.UniMiB_SHAR.unimib_shar_adl_load_dataset import unimib_load_dataset
 #from load_data_time_series_dev.HAR.e4_wristband_Nov2019.e4_load_dataset import e4_load_dataset
 from load_data_time_series_dev.HAR.MobiAct.mobiact_adl_load_dataset import mobiact_adl_load_dataset
-from scipy.stats import wasserstein_distance
-from scipy.spatial.distance import cdist
+from utils.add_nar import add_nar_from_array
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import pandas as pd
 import torch
@@ -40,7 +41,16 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 #TensorFlow -> channels last
 #PyTorch -> channels first (they get swapped in the feature extractors)
 
-results = {
+low_noise_results = {
+    'Features'          : [],
+    '# Mislabeled'      : [],
+    'P(mis)'            : [],
+    'P(mis|correct)'    : [],
+    'P(mis|mislabeled'  : [],
+}
+
+high_noise_results = {
+    'Features'          : [],
     '# Mislabeled'      : [],
     'P(mis)'            : [],
     'P(mis|correct)'    : [],
@@ -62,9 +72,38 @@ if __name__ == '__main__':
         else:
             flattened_test = np.array([np.linalg.norm(i, axis=0) for i in X_test])
 
+        y_low_noise_train, low_noise_indexes_train, y_high_noise_train, high_noise_indexes_train = add_nar_from_array(y, len(y[0]))
+        y_low_noise_test, low_noise_indexes_test, y_high_noise_test, high_noise_indexes_test = add_nar_from_array(y_test, len(y_test[0]))
+
         print('Shape of X: ', X.shape)
         print('Shape of y: ', y.shape)
+        print('Shape of low noise y: ', y_low_noise_train.shape)
+        print('Shape of high noise y: ', y_high_noise_train.shape)
+        print("Number low noise train labels altered: ", len(low_noise_indexes_train))
+        print("Number high noise train labels altered: ", len(high_noise_indexes_train))
         print('Shape of flattened train X: ', flattened_train.shape)
         print('Shape of flattened test X: ', flattened_test.shape)
         print('Shape of X_test: ', X_test.shape)
         print('Shape of y_test: ', y_test.shape)
+        print('Shape of low noise y_test: ', y_low_noise_test.shape)
+        print('Shape of high noise y_test: ', y_high_noise_test.shape)
+        print("Number low noise test labels altered: ", len(low_noise_indexes_test))
+        print("Number high noise test labels altered: ", len(high_noise_indexes_test))
+
+        if(run_trad):        
+            from utils.ts_feature_toolkit import get_features_for_set as get_trad_features
+            train_features = get_trad_features(np.reshape(flattened_train, (flattened_train.shape[0], flattened_train.shape[1])))
+            test_features = get_trad_features(np.reshape(flattened_test, (flattened_test.shape[0], flattened_test.shape[1])))
+            print('Shape of Traditional Features: ', train_features.shape)
+            model = KNeighborsClassifier(n_neighbors=3)
+            model.fit(train_features, y_low_noise_train)
+            y_pred = model.predict(test_features)
+            print("Trad accuracy low noise: ", accuracy_score(y_test, y_pred))
+
+    result_gram = pd.DataFrame.from_dict(low_noise_results)
+    result_gram.to_csv('src/results/experiment3_low_noise_dataframe.csv')
+    print(result_gram.to_string())
+
+    result_gram = pd.DataFrame.from_dict(high_noise_results)
+    result_gram.to_csv('src/results/experiment3_high_noise_dataframe.csv')
+    print(result_gram.to_string())
--- src/utils/add_nar.py
+++ src/utils/add_nar.py
@@ -6,6 +6,8 @@
 import numpy as np
 from random import randint
 import os
+from tensorflow.keras.utils import to_categorical
+
 
 """
 Noise at Random-> The mislabeling rate is influenced by class
@@ -14,7 +16,7 @@ Mislabel only instances from the majority class. The total mislabeling rate of t
 low noise label set will be 5% and the total mislabeling rate of the high noise
 set will be 10%.
 """
-def add_nar(clean_labels, filename, num_classes):
+def add_nar_from_file(clean_labels, filename, num_classes):
     low_noise_labels = open(filename + '_nar5.csv', 'w+')
     high_noise_labels = open(filename + '_nar10.csv', 'w+')
     low_indexes = open(filename + '_nar5_indexes.csv', 'w+')
@@ -67,3 +69,74 @@ def add_nar(clean_labels, filename, num_classes):
     os.system('cat {} | wc -l'.format(filename + '_nar5.csv'))
     print('Lines written to high noise file: ')
     os.system('cat {} | wc -l'.format(filename + '_nar10.csv'))
+
+def add_nar_from_array(clean_labels, num_classes):
+    low_noise_labels = []
+    high_noise_labels = []
+    low_indexes = []
+    high_indexes = []
+
+    total_counter = 0
+    l_flipped_counter = 0
+    h_flipped_counter = 0
+
+    if clean_labels.ndim > 1:
+        return_type = "one hot"
+        clean_labels = np.argmax(clean_labels, axis=-1)
+    else:
+        return_type = "flat"
+
+    counts = [np.count_nonzero(clean_labels==i) for i in range(num_classes)]
+    print("Label counts in add_nar: ", counts)
+    MAJ_LABEL = int(np.argmax(counts))
+    MIN_LABEL = int(np.argmin(counts))
+
+    assert MAJ_LABEL != MIN_LABEL, "Calculating class imbalance has gone horribly wrong"
+
+    imbalance = len(clean_labels)/counts[MAJ_LABEL]
+
+    assert imbalance < 10, "ERROR: imbalance is to high for NAR"
+
+    for i,l in enumerate(clean_labels):
+        total_counter += 1
+        if l==MAJ_LABEL and randint(0,100)<5*imbalance:
+            low_noise_labels.append(MIN_LABEL)
+            low_indexes.append(i)
+            l_flipped_counter += 1
+        else:
+            low_noise_labels.append(l)
+
+        if l==MAJ_LABEL and randint(0,100)<10*imbalance:
+            high_noise_labels.append(MIN_LABEL)
+            high_indexes.append(i)
+            h_flipped_counter += 1
+        else:
+            high_noise_labels.append(l)
+
+
+
+    #sanity checks
+    print('---NAR---')
+    print('Major label: ', MAJ_LABEL)
+    print('Minor label: ', MIN_LABEL)
+    print('Class imbalance: ', counts[MAJ_LABEL]/(counts[MIN_LABEL] if counts[MIN_LABEL] != 0 else 1))
+    print('Total labels processed: ', total_counter)
+    print('Low noise labels flipped: ', l_flipped_counter)
+    print('High noise labels flipped: ', h_flipped_counter) 
+
+    if return_type == "one hot":
+        print("Returning one hot labels")
+        return (
+            to_categorical(low_noise_labels),
+            low_indexes,
+            to_categorical(high_noise_labels),
+            high_indexes
+        )
+    else:
+        print("Returning flat labels")
+        return (
+               low_noise_labels,
+                low_indexes,
+               high_noise_labels,
+                high_indexes
+        )
